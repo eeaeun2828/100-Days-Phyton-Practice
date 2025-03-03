92089befc33f91cd15f77b1270ac354fc6a3a689
Eve Park <119627333+eeaeun2828@users.noreply.github.com>
2025-03-03 16:37:24 +0000

    Add files via upload

---
+++ task.py
@@ -0,0 +1,7 @@
+# Write your code below this line 👇
+
+city = input("Where is the city you were born? ")
+
+pet = input("What is your first pet's name? ")
+
+print("Here is your band name: " + city + " " + pet)
